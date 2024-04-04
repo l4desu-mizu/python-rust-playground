5d8057832ef9873669098efc938fbee9e36f9e25
Mizu <5565979+l4desu-mizu@users.noreply.github.com>
2024-04-04 22:42:16 +0200

    include sockets because I can

--- crates/core/src/fancy_complex_stuff.rs
+++ crates/core/src/fancy_complex_stuff.rs
@@ -4,11 +4,9 @@ use pyo3::prelude::*;
 pub fn execute_python() -> PyResult<String> {
     Python::with_gil(|py| {
         let module = PyModule::import(py, "my_python_lib.lib")?;
-        let res = module
-            .getattr("MyFancyStuff")?
-            .call1(("Hello",))?
-            .call_method0("do_stuff")?
-            .to_string();
+        let stuff = module.getattr("MyFancyStuff")?.call1(("World", "Hello"))?;
+        let res = stuff.call_method0("do_stuff")?.to_string();
+        stuff.call_method0("print_stuff")?;
         Ok(res)
     })
 }
--- my_python_lib/my_python_lib/lib.py
+++ my_python_lib/my_python_lib/lib.py
@@ -1,18 +1,40 @@
 import sys
+import socket
+import threading
 
 class MyFancyStuff:
 
-    def __init__(self, item):
+    def __init__(self, item1, item2):
         print(sys.executable)
-        self._item = item
+        self._item1 = item1
+        self._item2 = item2
 
     def do_stuff(self):
-        return self._item
+        return self._item1
 
     def print_stuff(self):
-        print(self._item)
+        with socket.socket() as s_in, socket.socket() as s_out:
+            s_in.bind(("127.0.0.1",0))
+            s_in.listen()
+            t = threading.Thread(target=run_recv, args=[s_out,s_in.getsockname()])
+            t.start()
+            s_write, _addr = s_in.accept()
+            s_write.send(self._item2.encode("utf8"))
+            s_write.close()
+            t.join()
 
+def run_recv(sock: socket.socket, addr):
+    sock.connect(addr)
+    res = ""
+    while True:
+        new=sock.recv(1024).decode("utf8")
+        if new:
+            res+=new
+        else:
+            break
+    print(res)
 
 if __name__ == "__main__":
-    MyFancyStuff("Hello").print_stuff()
-    MyFancyStuff("World").print_stuff()
+    stuff = MyFancyStuff("Hello", "World")
+    print(stuff.do_stuff())
+    stuff.print_stuff()
